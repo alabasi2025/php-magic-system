e800b1625cf8c67e8a95729210cf6552a685b01c
alabasi2025 <238506735+alabasi2025@users.noreply.github.com>
2025-12-02 04:46:46 -0500

    chore: تحديث رقم الإصدار إلى v2.9.8

--- config/version.php
+++ config/version.php
@@ -12,7 +12,7 @@ return [
     | All other files should reference this config value.
     |
     */
-    'number' => 'v2.9.7',
+    'number' => 'v2.9.8',
     /*
     |--------------------------------------------------------------------------
     | Version Name (Optional)
