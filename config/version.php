63bf78401772bae77adef70fef92cc3737b91453
alabasi2025 <238506735+alabasi2025@users.noreply.github.com>
2025-12-02 07:25:34 -0500

    chore: تحديث الإصدار إلى v2.10.11

--- config/version.php
+++ config/version.php
@@ -12,7 +12,7 @@ return [
     | All other files should reference this config value.
     |
     */
-    'number' => 'v2.10.10',
+    'number' => 'v2.10.11',
     /*
     |--------------------------------------------------------------------------
     | Version Name (Optional)
