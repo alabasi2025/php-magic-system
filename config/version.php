bb3cdcf4e8a392fb71a60890786949c0a1839a0a
alabasi2025 <238506735+alabasi2025@users.noreply.github.com>
2025-12-02 07:10:13 -0500

    chore: تحديث الإصدار إلى v2.10.6

--- config/version.php
+++ config/version.php
@@ -12,7 +12,7 @@ return [
     | All other files should reference this config value.
     |
     */
-    'number' => 'v2.10.5',
+    'number' => 'v2.10.6',
     /*
     |--------------------------------------------------------------------------
     | Version Name (Optional)
