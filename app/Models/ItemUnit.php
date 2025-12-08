12a3bd0ba909cb29ab3cd668351488ad62af217a
alabasi2025 <238506735+alabasi2025@users.noreply.github.com>
2025-12-08 13:01:56 -0500

    fix: Remove SoftDeletes from inventory models temporarily

--- app/Models/ItemUnit.php
+++ app/Models/ItemUnit.php
@@ -25,7 +25,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class ItemUnit extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     /**
      * The table associated with the model.
--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -24,7 +24,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class Warehouse extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
